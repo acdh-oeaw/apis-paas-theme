b132535ccc17fe1b9af0f1babd0083ebf542207f
Matthias Schlögl <m.schloegl@gmail.com>
2020-11-05 16:32:20 +0100

    initial changes

--- paas-theme/templatetags/custom_tags.py
+++ paas-theme/templatetags/custom_tags.py
@@ -25,52 +25,60 @@ def place_count():
 
 @register.simple_tag
 def formated_daterange(startdate, enddate):
-    rangestring = ''
+    rangestring = ""
     if (startdate and startdate is not None) or (enddate and enddate is not None):
-        rangestring += '('
-        if (startdate and startdate is not None):
+        rangestring += "("
+        if startdate and startdate is not None:
             rangestring += startdate
-        if (enddate and enddate is not None):
-            rangestring += '-' + enddate
-        rangestring += ')'
+        if enddate and enddate is not None:
+            rangestring += "-" + enddate
+        rangestring += ")"
     return rangestring
 
 
 @register.simple_tag
 def normalize_facet(facet, kind, url=None):
     norm_kind = {
-        'place_of_birth': 'Geburtsort',
-        'place_of_death': 'Sterbeort',
-        'career': 'Karriere',
-        'education': 'Ausbildung',
-        'profession': 'Beruf'
+        "place_of_birth": "Geburtsort",
+        "place_of_death": "Sterbeort",
+        "career": "Karriere",
+        "education": "Ausbildung",
+        "profession": "Beruf",
+        "comissions": "Komissionen",
     }
-    fac = facet.split(':')
-    fac[0] = fac[0].replace('_exact', '')
+    fac = facet.split(":")
+    fac[0] = fac[0].replace("_exact", "")
     if kind == "simple":
         return f"{norm_kind[facet]}"
     if kind == "name":
         return f"{norm_kind[fac[0].lower()]}: '{fac[1]}'"
     elif kind == "filter":
-        url2 = re.sub(f"([\&\?]selected_facets={fac[0]}_exact)(:|%3A)({urllib.parse.quote(fac[1])})", '',url)
-        url2 = url2.replace('search&', 'search?')
+        url2 = re.sub(
+            f"([\&\?]selected_facets={fac[0]}_exact)(:|%3A)({urllib.parse.quote(fac[1])})",
+            "",
+            url,
+        )
+        url2 = url2.replace("search&", "search?")
         return url2
 
 
 @register.simple_tag
 def remove_facets(url):
-    url = re.subn('&selected_facets\=[^&]+', '', url)
+    url = re.subn("&selected_facets\=[^&]+", "", url)
     return url[0]
 
+
 @register.simple_tag
-def filter_facetfields(fields,fieldname):
+def filter_facetfields(fields, fieldname):
     return fields[fieldname]
 
-@register.simple_tag
-def check_facet_selection(key,val,selectedfacets):
-    selectedfacet = f"{key}_exact:{val}" 
-    return selectedfacet in selectedfacets
 
 @register.simple_tag
-def filter_params(request,param):
+def check_facet_selection(key, val, selectedfacets):
+    selectedfacet = f"{key}_exact:{val}"
+    return selectedfacet in selectedfacets
+
+
+@register.simple_tag
+def filter_params(request, param):
     return request.GET.getlist(param)
--- paas-theme/views.py
+++ paas-theme/views.py
@@ -10,7 +10,12 @@ from webpage.views import get_imprint_url
 from .filters import PersonListFilter
 from .forms import PersonFilterFormHelper, PersonFacetedSearchForm
 from .tables import PersonTable, SearchResultTable
-from .utils import oebl_persons, get_daily_entries, get_featured_person, enrich_person_context
+from .utils import (
+    oebl_persons,
+    get_daily_entries,
+    get_featured_person,
+    enrich_person_context,
+)
 from haystack.generic_views import FacetedSearchView
 from haystack.forms import FacetedSearchForm
 from haystack.query import SearchQuerySet
@@ -24,7 +29,7 @@ from django_tables2.config import RequestConfig
 
 
 class ImprintView(TemplateView):
-    template_name = 'theme/imprint.html'
+    template_name = "theme/imprint.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -32,9 +37,11 @@ class ImprintView(TemplateView):
         r = requests.get(get_imprint_url())
 
         if r.status_code == 200:
-            context['imprint_body'] = "{}".format(r.text)
+            context["imprint_body"] = "{}".format(r.text)
         else:
-            context['imprint_body'] = """
+            context[
+                "imprint_body"
+            ] = """
             On of our services is currently not available. Please try it later or write an email to
             acdh@oeaw.ac.at; if you are service provide, make sure that you provided
             ACDH_IMPRINT_URL and REDMINE_ID
@@ -44,22 +51,22 @@ class ImprintView(TemplateView):
 
 class IndexView(TemplateView):
     model = Person
-    template_name = 'theme/index.html'
+    template_name = "theme/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['featured_pers'] = get_featured_person()
+        context["featured_pers"] = get_featured_person()
         enriched_context = get_daily_entries(context, oebl_persons)
-        enriched_context['random_entries'] = random.sample(list(oebl_persons), 2)
+        enriched_context["random_entries"] = random.sample(list(oebl_persons), 2)
         return enriched_context
 
 
 class AboutView(TemplateView):
-    template_name = 'theme/about.html'
+    template_name = "theme/about.html"
 
 
 class ContactView(TemplateView):
-    template_name = 'theme/contact.html'
+    template_name = "theme/contact.html"
 
 
 class PersonListView(GenericListView):
@@ -67,15 +74,14 @@ class PersonListView(GenericListView):
     filter_class = PersonListFilter
     formhelper_class = PersonFilterFormHelper
     table_class = PersonTable
-    template_name = 'theme/generic_list.html'
+    template_name = "theme/generic_list.html"
     init_columns = [
-        'name',
-        'first_name',
+        "name",
+        "first_name",
     ]
 
     def get_queryset(self, **kwargs):
-        self.filter = self.filter_class(self.request.GET,
-                                        queryset=oebl_persons)
+        self.filter = self.filter_class(self.request.GET, queryset=oebl_persons)
         self.filter.form.helper = self.formhelper_class()
         return self.filter.qs
 
@@ -83,12 +89,19 @@ class PersonListView(GenericListView):
 class PersonSearchView(FacetedSearchView):
     queryset = SearchQuerySet()
     form_class = PersonFacetedSearchForm
-    facet_fields = ['place_of_birth', 'place_of_death', 'profession', 'education', 'career']
+    facet_fields = [
+        "place_of_birth",
+        "place_of_death",
+        "comissions",
+        "profession",
+        "education",
+        "career",
+    ]
 
 
 class SearchView(SingleTableMixin, PersonSearchView):
     table_class = SearchResultTable
-    template_name = 'theme/person_search.html'
+    template_name = "theme/person_search.html"
 
     def get_table_data(self):
         return self.queryset
@@ -96,21 +109,21 @@ class SearchView(SingleTableMixin, PersonSearchView):
 
 class PersonDetailView(DetailView):
     model = Person
-    template_name = 'theme/person_detail.html'
+    template_name = "theme/person_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         try:
-            context['prev'] = oebl_persons.filter(
-                id__lt=self.object.id
-            ).order_by("-id").first()
+            context["prev"] = (
+                oebl_persons.filter(id__lt=self.object.id).order_by("-id").first()
+            )
         except AttributeError:
-            context['prev'] = None
+            context["prev"] = None
         try:
-            context['next'] = oebl_persons.filter(id__gt=self.object.id).first()
+            context["next"] = oebl_persons.filter(id__gt=self.object.id).first()
         except AttributeError:
-            context['next'] = None
+            context["next"] = None
         enriched_context = enrich_person_context(self.object, context)
 
         return enriched_context
